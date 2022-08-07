4cc556e8065820031ed4f3f3186992de1af7d86a
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-08-07 14:18:26 +0200

    Nav bar ok

--- src/copmonents/Navbar.js
+++ src/copmonents/Navbar.js
@@ -1,57 +1,80 @@
-
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true)
-    const handleClick = () => setClick(!click)
-    const closeMobileMenu = () => setClick(false)
-    const showButton = () => {
-        if(window.innerWidth <= 960){
-            setButton(false)
-        }else{
-            setButton(true)
-        }
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
     }
-    window.addEventListener('resize', showButton)
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-            <Link to="/" className="navbar-logo">
-                DIAGTECH
-            </Link>
-            <div className='meniu-icon' onClick={handleClick}>
-                <i className={ click ? 'fas fa-times' : 'fas fa-bars'} />
-            </div>
-            <ul className={ click ? 'nav-menu active' : 'nav-menu'}>
-                <li className='nav-item'>
-                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                        HEM
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
-                        OM OSS
-                    </Link>
-                </li> 
-                <li className='nav-item'>
-                    <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
-                        TJÄNSTER
-                    </Link>
-                </li> 
-                <li className='nav-item'>
-                    <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
-                        KONTAKT
-                    </Link>
-                </li>  
-            </ul>
-            {button && <Button buttonStyle='btn--outline'>KONTAKT</Button>}
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+           DiagTech
+            <i class='fab fa-typo3' />
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                HEM
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/services'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                OM OSS
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                TJÄNSTER
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                KONTAKT
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>Kontakt</Button>}
         </div>
       </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
