75e03e51688426ea8a4aca29f22d940a7ec5a9dc
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-08-07 13:22:03 +0200

    Add button

--- src/copmonents/Navbar.js
+++ src/copmonents/Navbar.js
@@ -1,11 +1,20 @@
 
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from './Button';
 function Navbar() {
     const [click, setClick] = useState(false);
-
+    const [button, setButton] = useState(true)
     const handleClick = () => setClick(!click)
     const closeMobileMenu = () => setClick(false)
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false)
+        }else{
+            setButton(true)
+        }
+    }
+    window.addEventListener('resize', showButton)
   return (
     <>
       <nav className='navbar'>
@@ -38,6 +47,7 @@ function Navbar() {
                     </Link>
                 </li>  
             </ul>
+            {button && <Button buttonStyle='btn--outline'>KONTAKT</Button>}
         </div>
       </nav>
     </>
